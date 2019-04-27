948cd69b8839ed0f8476a50fd2e8640d56f07431
paulmasson <paulmasson@analyticphysics.com>
2019-04-26 18:00:06 -0700

    Add keyboard navigation

--- webvr-worlds/js/VRController.js
+++ webvr-worlds/js/VRController.js
@@ -10,15 +10,20 @@ THREE.VRController = function( camera ) {
 
   var direction = new THREE.Vector3();
 
+  this.key = '';
+  var that = this;
+
+  window.addEventListener( 'keydown', function( event ) { that.key = event.key; } );
+
   this.update = function() {
+
+    camera.getWorldDirection( direction );
 
     var gamepads = navigator.getGamepads();
 
     var gp = gamepads[0] ? gamepads[0] : null;
 
     if ( gp !== null && gp.buttons[0].pressed ) {
-
-      camera.getWorldDirection( direction );
 
       var n = Math.round( 2 * Math.atan2( gp.axes[1], gp.axes[0] ) / Math.PI );
 
@@ -38,6 +43,40 @@ THREE.VRController = function( camera ) {
           break;
         case -1:
           this.rig.position.add( direction.multiplyScalar( this.moveRate ) );
+          break;
+        default:
+
+      }
+
+    } else {
+
+      // keyboard navigation
+
+      switch( this.key ) {
+
+        case 'd':
+          this.rig.rotation.y -= this.turnRate;
+          this.key = '';
+          break;
+        case 'a':
+          this.rig.rotation.y += this.turnRate;
+          this.key = '';
+          break;
+        case 's':
+          this.rig.position.add( direction.multiplyScalar( -this.moveRate ) );
+          this.key = '';
+          break;
+        case 'w':
+          this.rig.position.add( direction.multiplyScalar( this.moveRate ) );
+          this.key = '';
+          break;
+        case 'ArrowUp':
+          this.rig.position.y += this.moveRate;
+          this.key = '';
+          break;
+        case 'ArrowDown':
+          this.rig.position.y -= this.moveRate;
+          this.key = '';
           break;
         default:
 
